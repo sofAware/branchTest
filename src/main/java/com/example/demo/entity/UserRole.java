68e23fb30615d440f8a78e84a30684b6fc8820cc
npole0103 <npole0103@naver.com>
2022-01-26 17:08:16 +0900

    userRole Collision ver2.0

--- src/main/java/com/example/demo/entity/UserRole.java
+++ src/main/java/com/example/demo/entity/UserRole.java
@@ -6,9 +6,9 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum UserRole {
-    asdSTUDENT("ROLE_STUDENT", "학생"),
-    afsfasfPROFESSOR("ROLE_PROFESSOR", "교수"),
-    ADafsafasfafsfafMIN("ROLE_ADMIN", "관리자");
+    asdSTUDasfasfsfENT("ROLE_STUDENT", "학생"),
+    afsfasfPROFESasfasfasfasfsafasfsfaSOR("ROLE_PROFESSOR", "교수"),
+    ADafsafasfafsfasdadasdasdasfasfasfasfasfasfasafMIN("ROLE_ADMIN", "관리자");
 
     private final String key;
     private final String title;
